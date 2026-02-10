96d2dc938e2e0de025eaea44db82bc928e05ab42
Yu Hsuan Lee <s9070292108small@gmail.com>
2026-02-09 20:36:16 -0800

    Add main function for Quicksort_NR performance test
    Implement main function to test Quicksort_NR with random data and measure sorting time.

---
+++ Assignment 2/Problem3/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <stdlib.h>
+#include <cstdio>
+#include <time.h>
+#include <chrono>
+
+
+#include "Quicksort_NR.h"
+
+using namespace std;
+using namespace std::chrono;
+
+int main()
+{
+    const int DATA_SIZE = 1000000;
+    int *data = new int[DATA_SIZE];
+
+    srand(time(0));
+    for (int i = 0; i < DATA_SIZE; i++) data[i] = rand();
+
+    Quicksort_NR qsorter(data, DATA_SIZE);
+
+    auto start_time = steady_clock::now();
+    qsorter.sort();
+    auto end_time = steady_clock::now();
+
+    cout << (qsorter.verify_sorted() ? "SORTED" : "ERROR") << endl;
+    long ms = duration_cast<milliseconds>(end_time - start_time).count();
+    printf("%20ld ms\n", ms);
+
+    delete[] data;
+    return 0;
+}

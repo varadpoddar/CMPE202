cc0de4be132b1e1f8787e4e6a12f865d4688ac8a
Varad <varad.kumar3@gmail.com>
2026-02-10 17:32:16 -0800

    team-ass-2-prob-3-main

--- Assignment 2/Problem3/Quicksort_MT.h
+++ Assignment 2/Problem3/Quicksort_MT.h
@@ -8,7 +8,7 @@ using namespace std;
 class Quicksort_MT
 {
 public:
-    Quicksort_MT(int * const d, const int s, const int tread_MT) : data(d), size(s), tread_MT(tread_MT) {};
+    Quicksort_MT(int *const d, const int s, const int thread_MT) : data(d), size(s), thread_MT(thread_MT) {};
 
     void sort() { sort_NR(); }
 
@@ -17,7 +17,7 @@ public:
 private:
     int *data;
     int size;
-    int tread_MT;
+    int thread_MT;
 
     void swap_values_at(const int index1, const int index2);
     int partition(const int left_index, const int right_index);
--- Assignment 2/Problem3/main.cpp
+++ Assignment 2/Problem3/main.cpp
@@ -12,26 +12,27 @@ using namespace std::chrono;
 int main()
 {
     const int DATA_SIZE = 1000000;
+    const int max_thread_MT = 24;
     int *data = new int[DATA_SIZE];
-	int max_thread = 24;
 
     srand(time(0));
-    for (int i = 0; i < DATA_SIZE; i++) data[i] = rand();
+    for (int i = 0; i < DATA_SIZE; i++)
+        data[i] = rand();
 
-	for (int thread_MT = 1; thread_MT <= max_thread; thread_MT++) {
-    	int *datacopy = new int[DATA_SIZE];
-		for(int j=0;j<DATA_SIZE;j++) {datacopy[j]=data[j]}
+        for (int threads = 1; threads <= max_thread_MT; threads++)
+    {
+        int *data_copy = new int[DATA_SIZE];
+        copy(data, data + DATA_SIZE, data_copy);
+        Quicksort_MT qsorter(data_copy, DATA_SIZE, threads);
 
-		Quicksort_MT qsorter(data, DATA_SIZE, thread_MT);
+        auto start_time = steady_clock::now();
+        qsorter.sort();
+        auto end_time = steady_clock::now();
 
-    	auto start_time = steady_clock::now();
-   	    qsorter.sort();
-    	auto end_time = steady_clock::now();
-
-    	cout << (qsorter.verify_sorted() ? "SORTED" : "ERROR") << endl;
-    	long ms = duration_cast<milliseconds>(end_time - start_time).count();
-    	printf("%20ld ms\n", ms);
-	}
+        cout << (qsorter.verify_sorted() ? "SORTED" : "ERROR") << endl;
+        long ms = duration_cast<milliseconds>(end_time - start_time).count();
+        printf("%20ld ms\n", ms);
+    }
 
     delete[] data;
     return 0;
